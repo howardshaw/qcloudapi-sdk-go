5f5330d2d48eb8db1033967c33748bce584d5343
howardxiao <howardxiao@tencent.com>
2019-11-14 20:38:53 +0800

    make clientError err public

--- common/errors.go
+++ common/errors.go
@@ -34,7 +34,7 @@ func (vae VersionAPIError) Error() string {
 
 type ClientError struct {
 	Message string
-	err     error
+	Err     error
 }
 
 func (ce ClientError) Error() string {
@@ -44,6 +44,6 @@ func (ce ClientError) Error() string {
 func makeClientError(err error) ClientError {
 	return ClientError{
 		Message: err.Error(),
-		err:     err,
+		Err:     err,
 	}
 }
